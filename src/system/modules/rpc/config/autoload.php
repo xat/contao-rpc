7eee0accf5fe8c9d2e79366c96b8f2896ac4177c
Simon Kusterer <simon@soped.com>
2012-12-31 19:06:27 +0100

    changed the directory structure. It should be easier to find specific classes now.

--- src/system/modules/rpc/config/autoload.php
+++ src/system/modules/rpc/config/autoload.php
@@ -15,89 +15,120 @@
  */
 ClassLoader::addClasses(array
 (
-
+	// root
 	'Contao\Rpc\RpcRunner' => 'system/modules/rpc/classes/RpcRunner.php',
+	'Contao\Rpc\RpcStandardMethods' => 'system/modules/rpc/classes/RpcStandardMethods.php',
 
-	'Contao\Rpc\IRpcDecoder' => 'system/modules/rpc/classes/IRpcDecoder.php',
-	'Contao\Rpc\IRpcEncoder' => 'system/modules/rpc/classes/IRpcEncoder.php',
-	'Contao\Rpc\IRpcDecryption' => 'system/modules/rpc/classes/IRpcDecryption.php',
-	'Contao\Rpc\IRpcDecrypter' => 'system/modules/rpc/classes/IRpcDecrypter.php',
-	'Contao\Rpc\IRpcEncrypter' => 'system/modules/rpc/classes/IRpcEncrypter.php',
+	// access
+	'Contao\Rpc\ERpcAccessorException' => 'system/modules/rpc/classes/access/ERpcAccessorException.php',
+	'Contao\Rpc\IRpcAccessor' => 'system/modules/rpc/classes/access/IRpcAccessor.php',
+	'Contao\Rpc\IRpcAccess' => 'system/modules/rpc/classes/access/IRpcAccess.php',
+	'Contao\Rpc\RpcBasicAccess' => 'system/modules/rpc/classes/access/RpcBasicAccess.php',
 
-	'Contao\Rpc\RpcBasicEncryption' => 'system/modules/rpc/classes/RpcBasicEncryption.php',
-	'Contao\Rpc\RpcContaoEncrypter' => 'system/modules/rpc/classes/RpcContaoEncrypter.php',
-	'Contao\Rpc\RpcUserLookup' => 'system/modules/rpc/classes/RpcUserLookup.php',
-	'Contao\Rpc\IRpcEncryption' => 'system/modules/rpc/classes/IRpcEncryption.php',
+	// accessors
+	'Contao\Rpc\RpcUserAccessor' => 'system/modules/rpc/classes/access/accessors/RpcUserAccessor.php',
+	'Contao\Rpc\RpcPublicAccessor' => 'system/modules/rpc/classes/access/accessors/RpcPublicAccessor.php',
+	'Contao\Rpc\RpcAuthenticatorAccessor' => 'system/modules/rpc/classes/access/accessors/RpcAuthenticatorAccessor.php',
+	'Contao\Rpc\RpcActiveAccessor' => 'system/modules/rpc/classes/access/accessors/RpcActiveAccessor.php',
+	'Contao\Rpc\RpcConfigAccessor' => 'system/modules/rpc/classes/access/accessors/RpcConfigAccessor.php',
+	'Contao\Rpc\RpcAdminAccessor' => 'system/modules/rpc/classes/access/accessors/RpcAdminAccessor.php',
+	'Contao\Rpc\RpcIpListAccessor' => 'system/modules/rpc/classes/access/accessors/RpcIpListAccessor.php',
+	'Contao\Rpc\RpcSecureAccessor' => 'system/modules/rpc/classes/access/accessors/RpcSecureAccessor.php',
+	'Contao\Rpc\RpcEncryptionAccessor' => 'system/modules/rpc/classes/access/accessors/RpcEncryptionAccessor.php',
+	'Contao\Rpc\RpcFeAccessor' => 'system/modules/rpc/classes/access/accessors/RpcFeAccessor.php',
+	'Contao\Rpc\RpcBeAccessor' => 'system/modules/rpc/classes/access/accessors/RpcBeAccessor.php',
 
-	'Contao\Rpc\IRpcLookup' => 'system/modules/rpc/classes/IRpcLookup.php',
-	'Contao\Rpc\RpcTableLookup' => 'system/modules/rpc/classes/RpcTableLookup.php',
-	'Contao\Rpc\RpcBasicDecryption' => 'system/modules/rpc/classes/RpcBasicDecryption.php',
-	'Contao\Rpc\RpcContaoDecrypter' => 'system/modules/rpc/classes/RpcContaoDecrypter.php',
-	'Contao\Rpc\RpcHelpers' => 'system/modules/rpc/classes/RpcHelpers.php',
+	// authentication
+	'Contao\Rpc\IRpcAuthenticate' => 'system/modules/rpc/classes/authentication/IRpcAuthenticate.php',
+	'Contao\Rpc\RpcBasicAuthentication' => 'system/modules/rpc/classes/authentication/RpcBasicAuthentication.php',
+	'Contao\Rpc\IRpcAuthenticator' => 'system/modules/rpc/classes/authentication/IRpcAuthenticator.php',
 
-	'Contao\Rpc\JsonRpcDecoder' => 'system/modules/rpc/classes/JsonRpcDecoder.php',
-	'Contao\Rpc\JsonRpcEncoder' => 'system/modules/rpc/classes/JsonRpcEncoder.php',
-	'Contao\Rpc\RpcSetupFactory' => 'system/modules/rpc/classes/RpcSetupFactory.php',
-	'Contao\Rpc\TRpcSetup' => 'system/modules/rpc/classes/TRpcSetup.php',
+	// authenticators
+	'Contao\Rpc\RpcBeCredentialsAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcBeCredentialsAuthenticator.php',
+	'Contao\Rpc\RpcFeCredentialsAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcFeCredentialsAuthenticator.php',
+	'Contao\Rpc\RpcCredentialsAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcCredentialsAuthenticator.php',
+	'Contao\Rpc\RpcBeApikeyAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcBeApikeyAuthenticator.php',
+	'Contao\Rpc\RpcFeApikeyAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcFeApikeyAuthenticator.php',
+	'Contao\Rpc\RpcApikeyAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcApikeyAuthenticator.php',
+	'Contao\Rpc\RpcBeHashAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcBeHashAuthenticator.php',
+	'Contao\Rpc\RpcFeHashAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcFeHashAuthenticator.php',
+	'Contao\Rpc\RpcHashAuthenticator' => 'system/modules/rpc/classes/authentication/authenticators/RpcHashAuthenticator.php',
 
-	'Contao\Rpc\IRpcRequest' => 'system/modules/rpc/classes/IRpcRequest.php',
-	'Contao\Rpc\RpcRequest' => 'system/modules/rpc/classes/RpcRequest.php',
-	'Contao\Rpc\JsonRpcRequest' => 'system/modules/rpc/classes/JsonRpcRequest.php',
-	'Contao\Rpc\RpcBackendUser' => 'system/modules/rpc/classes/RpcBackendUser.php',
-	'Contao\Rpc\RpcFrontendUser' => 'system/modules/rpc/classes/RpcFrontendUser.php',
-	'Contao\Rpc\IRpcProvider' => 'system/modules/rpc/classes/IRpcProvider.php',
-	'Contao\Rpc\IRpcResponse' => 'system/modules/rpc/classes/IRpcResponse.php',
-	'Contao\Rpc\RpcResponse' => 'system/modules/rpc/classes/RpcResponse.php',
-	'Contao\Rpc\TRpcUser' => 'system/modules/rpc/classes/TRpcUser.php',
+	// decoder
+	'Contao\Rpc\IRpcDecoder' => 'system/modules/rpc/classes/decoder/IRpcDecoder.php',
 
-	'Contao\Rpc\IRpcField' => 'system/modules/rpc/classes/IRpcField.php',
-	'Contao\Rpc\RpcPostField' => 'system/modules/rpc/classes/RpcPostField.php',
-	'Contao\Rpc\IRpcInput' => 'system/modules/rpc/classes/IRpcInput.php',
-	'Contao\Rpc\RpcBasicInput' => 'system/modules/rpc/classes/RpcBasicInput.php',
-	'Contao\Rpc\IRpcOutput' => 'system/modules/rpc/classes/IRpcOutput.php',
-	'Contao\Rpc\RpcBasicOutput' => 'system/modules/rpc/classes/RpcBasicOutput.php',
-	'Contao\Rpc\IRpcResponsible' => 'system/modules/rpc/classes/IRpcResponsible.php',
-	'Contao\Rpc\RpcBasicResponsibility' => 'system/modules/rpc/classes/RpcBasicResponsibility.php',
-	'Contao\Rpc\IRpcSetup' => 'system/modules/rpc/classes/IRpcSetup.php',
-	'Contao\Rpc\IRpcSetInput' => 'system/modules/rpc/classes/IRpcSetInput.php',
-	'Contao\Rpc\TRpcSetInput' => 'system/modules/rpc/classes/TRpcSetInput.php',
+	// decoders
+	'Contao\Rpc\JsonRpcDecoder' => 'system/modules/rpc/classes/decoder/decoders/JsonRpcDecoder.php',
 
-	'Contao\Rpc\IRpcAuthenticate' => 'system/modules/rpc/classes/IRpcAuthenticate.php',
-	'Contao\Rpc\RpcBasicAuthentication' => 'system/modules/rpc/classes/RpcBasicAuthentication.php',
-	'Contao\Rpc\IRpcAuthenticator' => 'system/modules/rpc/classes/IRpcAuthenticator.php',
-	'Contao\Rpc\RpcBeCredentialsAuthenticator' => 'system/modules/rpc/classes/RpcBeCredentialsAuthenticator.php',
-	'Contao\Rpc\RpcFeCredentialsAuthenticator' => 'system/modules/rpc/classes/RpcFeCredentialsAuthenticator.php',
-	'Contao\Rpc\RpcCredentialsAuthenticator' => 'system/modules/rpc/classes/RpcCredentialsAuthenticator.php',
-	'Contao\Rpc\RpcBeApikeyAuthenticator' => 'system/modules/rpc/classes/RpcBeApikeyAuthenticator.php',
-	'Contao\Rpc\RpcFeApikeyAuthenticator' => 'system/modules/rpc/classes/RpcFeApikeyAuthenticator.php',
-	'Contao\Rpc\RpcApikeyAuthenticator' => 'system/modules/rpc/classes/RpcApikeyAuthenticator.php',
-	'Contao\Rpc\RpcBeHashAuthenticator' => 'system/modules/rpc/classes/RpcBeHashAuthenticator.php',
-	'Contao\Rpc\RpcFeHashAuthenticator' => 'system/modules/rpc/classes/RpcFeHashAuthenticator.php',
-	'Contao\Rpc\RpcHashAuthenticator' => 'system/modules/rpc/classes/RpcHashAuthenticator.php',
+	// decryption
+	'Contao\Rpc\IRpcDecryption' => 'system/modules/rpc/classes/decryption/IRpcDecryption.php',
+	'Contao\Rpc\IRpcDecrypter' => 'system/modules/rpc/classes/decryption/IRpcDecrypter.php',
+	'Contao\Rpc\RpcBasicDecryption' => 'system/modules/rpc/classes/decryption/RpcBasicDecryption.php',
 
-	'Contao\Rpc\RpcUserAccessor' => 'system/modules/rpc/classes/RpcUserAccessor.php',
-	'Contao\Rpc\RpcPublicAccessor' => 'system/modules/rpc/classes/RpcPublicAccessor.php',
-	'Contao\Rpc\RpcAuthenticatorAccessor' => 'system/modules/rpc/classes/RpcAuthenticatorAccessor.php',
-	'Contao\Rpc\RpcActiveAccessor' => 'system/modules/rpc/classes/RpcActiveAccessor.php',
-	'Contao\Rpc\RpcConfigAccessor' => 'system/modules/rpc/classes/RpcConfigAccessor.php',
-	'Contao\Rpc\RpcAdminAccessor' => 'system/modules/rpc/classes/RpcAdminAccessor.php',
-	'Contao\Rpc\RpcIpListAccessor' => 'system/modules/rpc/classes/RpcIpListAccessor.php',
-	'Contao\Rpc\RpcSecureAccessor' => 'system/modules/rpc/classes/RpcSecureAccessor.php',
-	'Contao\Rpc\RpcEncryptionAccessor' => 'system/modules/rpc/classes/RpcEncryptionAccessor.php',
-	'Contao\Rpc\RpcFeAccessor' => 'system/modules/rpc/classes/RpcFeAccessor.php',
-	'Contao\Rpc\RpcBeAccessor' => 'system/modules/rpc/classes/RpcBeAccessor.php',
-	'Contao\Rpc\RpcBasicAccess' => 'system/modules/rpc/classes/RpcBasicAccess.php',
-	'Contao\Rpc\IRpcAccessor' => 'system/modules/rpc/classes/IRpcAccessor.php',
-	'Contao\Rpc\IRpcAccess' => 'system/modules/rpc/classes/IRpcAccess.php',
+	// decrypters
+	'Contao\Rpc\RpcContaoDecrypter' => 'system/modules/rpc/classes/decryption/decrypters/RpcContaoDecrypter.php',
 
+	// encoder
+	'Contao\Rpc\IRpcEncoder' => 'system/modules/rpc/classes/encoder/IRpcEncoder.php',
+
+	// encoders
+	'Contao\Rpc\JsonRpcEncoder' => 'system/modules/rpc/classes/encoder/encoders/JsonRpcEncoder.php',
+
+	// encryption
+	'Contao\Rpc\IRpcEncrypter' => 'system/modules/rpc/classes/encryption/IRpcEncrypter.php',
+	'Contao\Rpc\RpcBasicEncryption' => 'system/modules/rpc/classes/encryption/RpcBasicEncryption.php',
+	'Contao\Rpc\IRpcEncryption' => 'system/modules/rpc/classes/encryption/IRpcEncryption.php',
+
+	// encrypters
+	'Contao\Rpc\RpcContaoEncrypter' => 'system/modules/rpc/classes/encryption/encrypters/RpcContaoEncrypter.php',
+
+	// input
+	'Contao\Rpc\IRpcField' => 'system/modules/rpc/classes/input/IRpcField.php',
+	'Contao\Rpc\IRpcInput' => 'system/modules/rpc/classes/input/IRpcInput.php',
+	'Contao\Rpc\RpcBasicInput' => 'system/modules/rpc/classes/input/RpcBasicInput.php',
+	'Contao\Rpc\IRpcSetInput' => 'system/modules/rpc/classes/input/IRpcSetInput.php',
+	'Contao\Rpc\TRpcSetInput' => 'system/modules/rpc/classes/input/TRpcSetInput.php',
+
+	// fields
+	'Contao\Rpc\RpcPostField' => 'system/modules/rpc/classes/input/fields/RpcPostField.php',
+
+	// lookup
+	'Contao\Rpc\IRpcLookup' => 'system/modules/rpc/classes/lookup/IRpcLookup.php',
+
+	// lookups
+	'Contao\Rpc\RpcTableLookup' => 'system/modules/rpc/classes/lookup/lookups/RpcTableLookup.php',
+	'Contao\Rpc\RpcUserLookup' => 'system/modules/rpc/classes/lookup/lookups/RpcUserLookup.php',
+
+	// misc
+	'Contao\Rpc\RpcHelpers' => 'system/modules/rpc/classes/misc/RpcHelpers.php',
+	'Contao\Rpc\RpcSetupFactory' => 'system/modules/rpc/classes/misc/RpcSetupFactory.php',
+	'Contao\Rpc\TRpcSetup' => 'system/modules/rpc/classes/misc/TRpcSetup.php',
+	'Contao\Rpc\RpcBackendUser' => 'system/modules/rpc/classes/misc/RpcBackendUser.php',
+	'Contao\Rpc\RpcFrontendUser' => 'system/modules/rpc/classes/misc/RpcFrontendUser.php',
+	'Contao\Rpc\TRpcUser' => 'system/modules/rpc/classes/misc/TRpcUser.php',
+	'Contao\Rpc\IRpcSetup' => 'system/modules/rpc/classes/misc/IRpcSetup.php',
+	'Contao\Rpc\RpcRegistry' => 'system/modules/rpc/classes/misc/RpcRegistry.php',
+
+	// output
+	'Contao\Rpc\IRpcOutput' => 'system/modules/rpc/classes/output/IRpcOutput.php',
+	'Contao\Rpc\RpcBasicOutput' => 'system/modules/rpc/classes/output/RpcBasicOutput.php',
+
+	// request
+	'Contao\Rpc\IRpcRequest' => 'system/modules/rpc/classes/request/IRpcRequest.php',
+	'Contao\Rpc\RpcRequest' => 'system/modules/rpc/classes/request/RpcRequest.php',
+	'Contao\Rpc\JsonRpcRequest' => 'system/modules/rpc/classes/request/JsonRpcRequest.php',
+
+	// response
+	'Contao\Rpc\IRpcResponse' => 'system/modules/rpc/classes/response/IRpcResponse.php',
+	'Contao\Rpc\RpcResponse' => 'system/modules/rpc/classes/response/RpcResponse.php',
+
+	// output
+	'Contao\Rpc\IRpcResponsible' => 'system/modules/rpc/classes/responsibility/IRpcResponsible.php',
+	'Contao\Rpc\RpcBasicResponsibility' => 'system/modules/rpc/classes/responsibility/RpcBasicResponsibility.php',
+
+	// models
 	// TODO: Namespace caused Problems: Fatal error: Class 'RpcModel' not found in /home/sope/projects/contaorpc.dev.soped.lan/www/src/system/modules/rpc/dca/tl_rpc.php on line 152
 	'RpcModel' => 'system/modules/rpc/models/RpcModel.php',
 	'RpcIpListItemModel' => 'system/modules/rpc/models/RpcIpListItemModel.php',
 	'RpcConfigurationModel' => 'system/modules/rpc/models/RpcConfigurationModel.php',
-
-	'Contao\Rpc\RpcRegistry' => 'system/modules/rpc/classes/RpcRegistry.php',
-
-	'Contao\Rpc\ERpcAccessorException' => 'system/modules/rpc/classes/ERpcAccessorException.php',
-
-	'Contao\Rpc\RpcStandardMethods' => 'system/modules/rpc/classes/RpcStandardMethods.php'
 ));
