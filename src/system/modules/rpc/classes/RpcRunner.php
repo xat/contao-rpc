1da1f5ca2fd2d381bcbf4ca2ce2769957199c919
Simon Kusterer <simon@soped.com>
2013-01-01 21:05:27 +0100

    Added a couple of hooks ( closes #14 )

--- src/system/modules/rpc/classes/RpcRunner.php
+++ src/system/modules/rpc/classes/RpcRunner.php
@@ -58,6 +58,8 @@ class RpcRunner
 
 		if (!$blnFoundProvider)
 		{
+			\Hooky::trigger('rpc_no_provider', $this);
+
 			// No Provider found. abort.
 			header('HTTP/1.1 400 Bad Request');
 			die('Bad Request');
@@ -68,6 +70,7 @@ class RpcRunner
 
 		RpcRegistry::set('provider', $strProvider);
 		RpcRegistry::set('input', $objInput);
+		\Hooky::trigger('rpc_find_post', $this);
 
 		return $this;
 	}
@@ -93,12 +96,15 @@ class RpcRunner
 		$objAuthentication  = RpcSetupFactory::create($this->arrSettings['authentication']);
 		if (!($strAuthType = $objAuthentication->authenticate()))
 		{
+			\Hooky::trigger('rpc_access_denied', $this);
+
 			// Abort on a failed authentication.
 			header('HTTP/1.1 403 Access Denied');
 			die('Access Denied');
 		}
 
 		define('RPC_AUTH', $strAuthType);
+		\Hooky::trigger('rpc_authentication_post', $this, $strAuthType);
 
 		return $this;
 	}
@@ -110,6 +116,7 @@ class RpcRunner
 	{
 		$objDecoder = RpcSetupFactory::create($this->arrSettings['decoder']);
 		$this->arrPairs = $objDecoder->decode();
+		\Hooky::trigger('rpc_decode_post', $this);
 
 		return $this;
 	}
@@ -132,21 +139,27 @@ class RpcRunner
 					if (!$objAccess->hasAccess($objPair->request->getMethodName()))
 					{
 						$objPair->response->setErrorType(RpcResponse::ACCESS_DENIED);
+						\Hooky::trigger('rpc_run_method_access_denied', $this, $objPair);
 						continue;
 					}
 				} catch (ERpcAccessorException $e)
 				{
 					$objPair->response->setError(2, $e->getMessage());
+					\Hooky::trigger('rpc_run_method_exception', $this, $e);
 					continue;
 				}
 
+				\Hooky::trigger('rpc_run_method_pre', $this, $objPair);
 				$arrRpc = $GLOBALS['RPC']['methods'][$objPair->request->getMethodName()];
 
 				// Run the actual RPC Method and pass in
 				// an Request and an Response object
 				(new $arrRpc['call'][0])->$arrRpc['call'][1]($objPair->request, $objPair->response, $this);
+				\Hooky::trigger('rpc_run_method_post', $this, $objPair);
 			}
 		}
+
+		\Hooky::trigger('rpc_run_post', $this);
 
 		return $this;
 	}
@@ -160,6 +173,7 @@ class RpcRunner
 		// transform all RPC Reponses into something
 		// we can send back to the user.
 		$this->strResponse = $objEncoder->encode($this->arrPairs);
+		\Hooky::trigger('rpc_encode_post', $this);
 
 		return $this;
 	}
@@ -175,6 +189,7 @@ class RpcRunner
 		{
 			$this->strResponse = $strEncrypted;
 		}
+		\Hooky::trigger('rpc_encrypt_post', $this);
 
 		return $this;
 	}
@@ -186,6 +201,7 @@ class RpcRunner
 	public function output()
 	{
 		$objOutput = RpcSetupFactory::create($this->arrSettings['output']);
+		\Hooky::trigger('rpc_output_pre', $this);
 		$objOutput->output($this->strResponse);
 
 		return $this;
